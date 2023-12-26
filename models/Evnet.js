624b59f80e16145d88e983f98efe139c35065293
engkemo1 <kemoeng40@gmail.com>
2023-12-26 18:29:27 +0200

    new

--- models/Evnet.js
+++ models/Evnet.js
@@ -1,18 +1,61 @@
 const mongoose = require('mongoose')
 const eventSchema = new mongoose.Schema({
-    title: {
+    name: {
         type: String,
         required: true
     },
-    description: {
+    phone: {
         type: String,
         required: true
     },
-    location: {
+    address: {
         type: String,
+        required: true
+    },
+    prize: {
+        type: Date,
+        required: true
+    },
+   amount_paid: {
+        type: Date,
+        required: true
+    },
+    Remaining_amount : {
+        type: Date,
         required: true
     },
     date: {
+        type: Date,
+        required: true
+    },
+    prize: {
+        type: Date,
+        required: true
+    },
+    num_dresses: {
+        type: Date,
+        required: true
+    },  length: {
+        type: Date,
+        required: true
+    },
+    chest_length : {
+        type: Date,
+        required: true
+    },  neck: {
+        type: Date,
+        required: true
+    },  hand_length: {
+        type: Date,
+        required: true
+    },  k_length: {
+        type: Date,
+        required: true
+    },  m_length: {
+        type: Date,
+        required: true
+    },
+    details: {
         type: Date,
         required: true
     },
