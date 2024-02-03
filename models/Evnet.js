7b28b209bb49cfef595e6f54aef4e6bb7d265400
engkemo1 <kemoeng40@gmail.com>
2024-02-03 15:33:31 +0200

    new

--- models/Evnet.js
+++ models/Evnet.js
@@ -1,3 +1,4 @@
+const { Double } = require('mongodb')
 const mongoose = require('mongoose')
 const eventSchema = new mongoose.Schema({
     name: {
@@ -14,58 +15,44 @@ const eventSchema = new mongoose.Schema({
     },
     prize: {
         type: Number,
-        required: true
     },
    amount_paid: {
         type: Number,
-        required: true
     },
     Remaining_amount : {
         type: Number,
-        required: true
     },
     date: {
         type: Date,
-        required: true
     },
     prize: {
         type: Number,
-        required: true
     },
     num_dresses: {
         type: Number,
-        required: true
     },  length: {
         type: Number,
-        required: true
     },
     chest_length : {
         type: Number,
-        required: true
     },
     ketf_length : {
         type: Number,
-        required: true
     }, 
    kom_length : {
         type: Number,
-        required: true
     }, 
     neck: {
         type: Number,
-        required: true
     }, 
      hand_length: {
         type: Number,
-        required: true
     }, 
      k_length: {
         type: Number,
-        required: true
     }, 
      m_length: {
         type: Number,
-        required: true
     },
     details: {
         type: String,
@@ -73,49 +60,64 @@ const eventSchema = new mongoose.Schema({
     glab: {
         
         type: String,
-        required: true
 
     },   
     yaqa: {
         type: String,
-        required: true
 
     },
        gayb: {
         type: String,
-        required: true
 
     },
        ganb: {
         type: String,
-        required: true
 
     },
     sadr: {
         type: String,
-        required: true
 
     },
     taqwera: {
         type: String,
-        required: true
 
-    },   details: {
-        type: String,
-        required: true
-
-    },
+    },  
     kom : {
         type: String,
-        required: true
     },
     created_at: {
         type: Date,
-        required: true
     },
+    delivery_time: {
+        type: Date,
+    },
+    type: {
+        type: String,
+    }, 
+    additional_type1: {
+        type: String,
+    },
+    additional_type2: {
+        type: String,
+    },
+    fyber_type: {
+        type: String,
+    },
+    quantity: {
+        type: Number,
+    },
+    worker_cost:{
+        type: Number,
+
+    },
+    total:{
+        type: Number,
+
+    },
+
+
     image:{
         type:String,
-        required:true
     }
 })
 
